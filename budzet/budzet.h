be087d64a67e35eec1c345107834c598505fbda6
Morekuuu <31134273+Morekuuu@users.noreply.github.com>
2025-12-21 10:41:28 +0100

    Refactor Budzet class methods and member variables

--- budzet/budzet.h
+++ budzet/budzet.h
@@ -9,48 +9,33 @@
 class Budzet
 {
     public:
-        std::string nazwa;
-        std::list<Konto> konta; // Lista kont wewnątrz tego budżetu
-
-        Budzet(std::string n) : nazwa(n) {}
+        Budzet(std::string n);
 
         // Metoda dodająca nową kwotę i miejsce do budżetu
-        void dodajSrodki(double kwota, std::string lokalizacja) {
-            konta.push_back(Konto(kwota, lokalizacja));
-        }
+        void dodajKonto(double kwota, std::string lokalizacja);
+
+        // Dodaje środki do wybranego konta
+        void zmianaŚrodków();
+
+        // Metoda usuwająca konto w budżecie wraz z środkami
+        void usunKonto();
 
         // Metoda obliczająca sumę w danym budżecie
-        double sumaCalkowita() const {
-            double suma = 0;
-            for (const auto& konto : konta) {
-                suma += konto.kwota;
-            }
-            return suma;
-        }
+        double sumaCalkowita() const;
 
         // Wyświetlanie szczegółów budżetu
-        void wyswietl() const
-        {
-            std::cout << "=== Budzet: " << nazwa << " ===" << std::endl;
-            if (konta.empty())
-            {
-                std::cout << "  (Brak srodkow)" << std::endl;
-            }
-            else
-            {
-                for (const auto& konto : konta)
-                {
-                    std::cout << "  - " << konto.lokalizacja << ": "
-                         << std::fixed << std::setprecision(2) << konto.kwota << " PLN" << std::endl;
-                }
-                std::cout << "  SUMA: " << std::fixed << std::setprecision(2) << sumaCalkowita() << " PLN" << std::endl;
-            }
-            std::cout << "-------------------------" << std::endl;
-        }
+        void wyswietl() const;
 
-        void wyswietl_dostepnosc() const
-        {
-            std::cout << nazwa << std::endl;
-            std::cout <<  "     środki budżetu: " << sumaCalkowita() << " PLN" << std::endl;
-        }
+        // Metoda na wyświetlenie nazwy oraz sumy środków budżetu
+        void wyswietl_dostepnosc() const;
+
+        std::string nazwaBudżetu()const;
+
+        int ileKont() const;
+
+        std::list<Konto> konta; // Lista kont wewnątrz tego budżetu
+
+    private:
+        std::string nazwa;
+
 };
