402b6613963573bd604e99000e6824809f468a50
Morekuuu <31134273+Morekuuu@users.noreply.github.com>
2025-12-24 11:48:53 +0100

    Add saldoZmiana method to Konto class
    Added a method to change the account balance.

--- budzet/konto.h
+++ budzet/konto.h
@@ -6,11 +6,14 @@ class Konto
 {
     public:
         Konto(std::string n, double s);
-        std::string nazwaZwrot(); //metoda zwracająca nazwę
-        double saldoZwrot(); //metoda zwracająca saldo
+        std::string nazwaZwrot() const; //metoda zwracająca nazwę
+        double saldoZwrot() const; //metoda zwracająca saldo
+
+        void saldoZmiana(double zmiana); //Metoda zmieniająca saldo
 
     private:
         std::string nazwa_;
         double saldo_;
+
 
 };
