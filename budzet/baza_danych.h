8919d5484849f80d90d9781e2d1a00fd45903529
Morekuuu <31134273+Morekuuu@users.noreply.github.com>
2025-12-26 12:09:21 +0100

    Add transaction logging functions to database header
    Added functions for transaction logging and date retrieval.

--- budzet/baza_danych.h
+++ budzet/baza_danych.h
@@ -4,3 +4,27 @@
 
 void zapiszBaze(const std::list<Budzet>& lista, const std::list<std::string>& konta);
 void wczytajBaze(std::list<Budzet>& lista, std::list<std::string>& konta);
+
+void zapisTransackji(int zdarzenie, std::string rodzaj1, std::string rodzaj2);
+void odczytTransakcji();
+
+std::string pobierzDate();
+
+
+
+
+//Transakcje
+/*
+ *typy logów:
+ *  <data> Dodano nowy budżet <Budżet>
+ *  <data> Usunięto nowy budżet <Budżet>
+ *  <data> Dodano nowe <konto> do budżetu <Budżet>
+ *  <data> Usunięto <konto> z budżetu <Budżet>
+ *  <data> Dodano nowe konto <nazwakonta> do list kont
+ *  <data> Usunięto konto <nazwakonta> do list kont
+ *  <data> odjęto środki z konta <konto> z budżetu <Budżet>
+ *  <data> dodano środki do konta <konto> w Budżecie <budżet>
+ *
+ *
+ *
+ */
