18f2ddcfc0bb57ddbfffa5c5ae175f52f969a764
Morekuuu <31134273+Morekuuu@users.noreply.github.com>
2025-12-26 12:12:06 +0100

    Add account types and additional attributes to Konto

--- budzet/konto.h
+++ budzet/konto.h
@@ -6,14 +6,19 @@ class Konto
 {
     public:
         Konto(std::string n, double s);
+        enum rodzaj {basic, saving, deposit, bonds, credit}; //definiowanie typów konta
+
         std::string nazwaZwrot() const; //metoda zwracająca nazwę
         double saldoZwrot() const; //metoda zwracająca saldo
 
         void saldoZmiana(double zmiana); //Metoda zmieniająca saldo
 
     private:
-        std::string nazwa_;
-        double saldo_;
-
+        std::string nazwa_; //zmienna nazwy konta
+        double saldo_; //zmienna salda konta
+        rodzaj rodzaj_; //zmienna rodzaju konta
+        double oprocentowanie_;
+        double okresCykluZmian_;
+        double okresTrwania_;
 
 };
