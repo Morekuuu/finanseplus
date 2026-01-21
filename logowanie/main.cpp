f2e9541425d64b549a53126f1e8fa31163ec3be2
Krzysztof8888 <k.mysior888@gmail.com>
2026-01-21 21:58:46 +0100

    Update main.cpp

--- logowanie/main.cpp
+++ logowanie/main.cpp
@@ -1,10 +1,16 @@
 #include "logowanie.h"
 #include <QApplication>
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+    //jesli dobre logowanie tworzy maina
     QApplication a(argc, argv);
-    Logowanie w;
-    w.show();
-    return a.exec();
+    Logowanie dlg;
+    if(dlg.exec()==QDialog::Accepted){
+        MainWindow w;
+        w.show();
+        return a.exec();
+    }
+return 0;
 }
