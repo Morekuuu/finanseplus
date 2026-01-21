03cd14a9f849826cb8b78c25daa22601a3bfda7a
Krzysztof8888 <k.mysior888@gmail.com>
2026-01-21 22:00:35 +0100

    Update logowanie.cpp
    Updated login functionality to use hashed passwords for authentication.

--- logowanie/logowanie.cpp
+++ logowanie/logowanie.cpp
@@ -1,8 +1,10 @@
 #include "logowanie.h"
 #include "ui_logowanie.h"
-#include "mainwindow.h"
 #include <QMessageBox>
+#include <QCryptographicHash>
+#include <QDebug>
 
+//tworzy pola i przyciski
 Logowanie::Logowanie(QWidget *parent)
     : QDialog(parent)
     , ui(new Ui::Logowanie)
@@ -15,17 +17,32 @@ Logowanie::~Logowanie()
     delete ui;
 }
 
+//hashowanie hasła
+static QString hashPassword(const QString &pass)
+{
+
+    const QString salt = "abc123xyz";
+    QByteArray data = (salt + pass).toUtf8();
+    QByteArray hash = QCryptographicHash::hash(data, QCryptographicHash::Sha256);
+    return hash.toHex();
+}
+
+
+//logowanie
 void Logowanie::on_zaloguj_clicked()
 {
     QString login = ui->login->text();
     QString pass  = ui->haslo->text();
+    QString passHash = hashPassword(pass);
+    QString correctHash = "5208d4270baced7070ccfc4948731b34cf8f3fa697398a0fa6b423a48dfa81b8";
 
-    if (login == "login" && pass == "hasło")
+    //poprawne dane
+    if (login == "login" && passHash == correctHash)
     {
-        MainWindow *m = new MainWindow();
-        m->show();
-        close();
+        accept();
     }
+
+    //błedne dane
     else
     {
         QMessageBox::warning(this, "Błąd", "Niepoprawny login lub hasło!");
@@ -33,6 +50,8 @@ void Logowanie::on_zaloguj_clicked()
         ui->haslo->clear();
     }
 }
+
+//pokaż/ukryj hasło
 void Logowanie::on_pokazHaslo_clicked()
 {
     if (ui->haslo->echoMode() == QLineEdit::Password)
