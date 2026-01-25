ec194f23ce144646a5a421a589dbe77024979130
Krzysztof8888 <k.mysior888@gmail.com>
2026-01-25 21:21:12 +0100

    Add files via upload

---
+++ finanse+/budzet/baza_danych.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <list>
+#include "budzet.h"
+#include "dlugi.h"
+
+void zapiszBaze(const std::list<Budzet>& lista, const std::list<std::string>& konta);
+void wczytajBaze(std::list<Budzet>& lista, std::list<std::string>& konta);
+
+void zapisTransackji(int zdarzenie, std::string rodzaj1, std::string rodzaj2);
+void odczytTransakcji();
+
+void zapiszDlugi(const std::list<Dlug>& lista);
+
+void wczytajDlugi(std::list<Dlug>& lista);
+
+std::string pobierzDate();
+
+
+
+
+//Transakcje
+/*
+ *typy logów:
+ *  <data> Dodano nowy budżet <Budżet>
+ *  <data> Usunięto nowy budżet <Budżet>
+ *  <data> Dodano nowe <konto> do budżetu <Budżet>
+ *  <data> Usunięto <konto> z budżetu <Budżet>
+ *  <data> Dodano nowe konto <nazwakonta> do list kont
+ *  <data> Usunięto konto <nazwakonta> do list kont
+ *  <data> odjęto środki z konta <konto> z budżetu <Budżet>
+ *  <data> dodano środki do konta <konto> w Budżecie <budżet>
+ *
+ *
+ *
+ */
---
+++ finanse+/budzet/mainwindow.h
@@ -0,0 +1,89 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTabWidget>
+#include <QTreeWidget>
+#include <QTableWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QComboBox>
+#include <QLineEdit>
+#include <QDateEdit>
+#include "budzet.h"
+#include "dlugi.h"
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    //Sloty dla Budżetów
+    void onDodajBudzet();
+    void onUsunBudzet();
+    void onDodajKonto();
+    void onUsunKonto();
+    void onZmienSrodki();
+
+    //Sloty dla Długów
+    void onDodajDlug();
+    void onSplacDlug();
+
+    //Sloty dla Historii
+    void onFiltrujHistorie();
+    void wczytajHistorieZCSV();
+
+    //Slot dla Raportu
+    void generujRaport();
+
+private:
+    // Listy
+    std::list<Budzet> listaBudzetow;
+    std::list<std::string> nazwyKont;
+    std::list<Dlug> listaDlugow;
+
+    // UI COMPONENTS
+    QTabWidget *mainTabWidget;
+
+    // Zakładka 1: Budżety
+    QWidget *tabBudzety;
+    QTreeWidget *treeBudzety;
+    QPushButton *btnDodajBudzet;
+    QPushButton *btnUsunBudzet;
+    QPushButton *btnDodajKonto;
+    QPushButton *btnUsunKonto;
+    QPushButton *btnZmienSrodki;
+    QLabel *lblSumaCalkowita;
+
+    // Zakładka 2: Długi
+    QWidget *tabDlugi;
+    QTableWidget *tableDlugi;
+    QPushButton *btnDodajDlug;
+    QPushButton *btnSplacDlug;
+
+    // Zakładka 3: Historia
+    QWidget *tabHistoria;
+    QTableWidget *tableHistoria;
+    QComboBox *comboFiltrRodzaj;
+    QLineEdit *txtSzukaj;
+    QLabel *lblSumaHistorii;
+
+    // Zakładka 4: Raport
+    QWidget *tabRaport;
+    QTableWidget *tableRaport;
+    QLabel *lblSumaRaport;
+
+    // Metody pomocnicze
+    void odswiezWidokBudzetow();
+    void odswiezWidokDlugow();
+    void setupUi();
+    void zapiszDane();
+};
+
+#endif
